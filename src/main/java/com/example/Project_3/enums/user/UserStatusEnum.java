99b4fd6df638d4330fdecd751eababaf047b5db7
tuanduong <phanduong2510@gmail.com>
2024-05-15 01:17:43 +0700

    change IdentityTypeEnum in update and add api switch status user

---
+++ src/main/java/com/example/Project_3/enums/user/UserStatusEnum.java
@@ -0,0 +1,16 @@
+package com.example.Project_3.enums.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+public enum UserStatusEnum {
+    ACTIVE(0, "Đang hoạt động"),
+    INACTIVE(1, "Tạm dừng hoạt động"),
+    DISABLED(2, "Tắt hoạt động");
+
+    private final Integer status;
+    private final String statusText;
+}
